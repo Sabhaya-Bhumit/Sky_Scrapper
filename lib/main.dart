e800c30af8e7a2232936a54c9d7d5b5ebd84ab13
bhumit sabhaya <113710907+Sabhaya-Bhumit@users.noreply.github.com>
2022-11-09 10:18:54 +0530

    Currency Converter  API

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,10 @@ List? all_cuntcies;
 Map? data;
 TextEditingController amount_controller = TextEditingController(text: "1");
 dynamic initalValue = "USD";
+int iosinitalValue = 0;
 dynamic intvalue = "INR";
+int iosintvalue = 0;
+
 TextStyle style = TextStyle(fontSize: 20);
 
 class home extends StatefulWidget {
@@ -57,147 +60,163 @@ class _homeState extends State<home> {
                 ),
                 child: SingleChildScrollView(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 100),
-                      Row(
-                        children: [
-                          Expanded(
+                      FutureBuilder(
+                        future: CurrenciAPIHelper.currenciAPIHelper.fectdat(
+                            API:
+                                "https://api.fastforex.io/fetch-all?api_key=3cc51ff263-08e3d3a87b-rkrecw"),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return Center(
+                                child: Text("Error is ${snapshot.error}"));
+                          } else if (snapshot.hasData) {
+                            all_cuntcies = snapshot.data;
+                            // Currencies? data = snapshot.data;
+                            // value = data;
+
+                            return Padding(
+                              padding: EdgeInsets.all(20),
                               child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              FutureBuilder(
-                                future: CurrenciAPIHelper.currenciAPIHelper.fectdat(
-                                    API:
-                                        "https://api.fastforex.io/fetch-all?api_key=3cc51ff263-08e3d3a87b-rkrecw"),
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasError) {
-                                    return Center(
-                                        child:
-                                            Text("Error is ${snapshot.error}"));
-                                  } else if (snapshot.hasData) {
-                                    all_cuntcies = snapshot.data;
-                                    // Currencies? data = snapshot.data;
-                                    // value = data;
-                                    return Column(
-                                      children: [
-                                        Text("Currency\nConverter"),
-                                        Text("From"),
-                                        Container(
-                                          height: 50,
-                                          width: double.infinity,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "\n\nCurrency\nConverter",
+                                    style: TextStyle(
+                                        fontSize: 40,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.lightBlue),
+                                  ),
+                                  SizedBox(height: 50),
+                                  Text(
+                                    "From : ",
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Container(
+                                    height: 100,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        border: Border.all(
+                                            color: Colors.black, width: 3)),
+                                    child: CupertinoPicker(
+                                      children: all_cuntcies!
+                                          .map((e) => Text("${e.key}"))
+                                          .toList(),
+                                      onSelectedItemChanged: (val) {
+                                        setState(() {
+                                          iosinitalValue = val;
+                                          initalValue =
+                                              all_cuntcies![iosinitalValue].key;
+                                        });
+                                      },
+                                      itemExtent: 35,
+                                    ),
+                                  ),
+                                  SizedBox(height: 30),
+                                  Text(
+                                    "To : ",
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Container(
+                                    height: 100,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        border: Border.all(
+                                            color: Colors.black, width: 3)),
+                                    child: CupertinoPicker(
+                                      children: all_cuntcies!
+                                          .map((e) => Text("${e.key}"))
+                                          .toList(),
+                                      onSelectedItemChanged: (val) {
+                                        setState(() {
+                                          iosintvalue = val;
+                                          intvalue =
+                                              all_cuntcies![iosintvalue].key;
+                                        });
+                                      },
+                                      itemExtent: 35,
+                                    ),
+                                  ),
+                                  SizedBox(height: 30),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Amount : ",
+                                        style: TextStyle(fontSize: 30),
+                                      ),
+                                      Expanded(
+                                        child: CupertinoTextField(
+                                          controller: amount_controller,
                                           decoration: BoxDecoration(
                                               borderRadius:
-                                                  BorderRadius.circular(20)),
-                                          child: DropdownButton<dynamic>(
-                                            value: initalValue,
-                                            items: all_cuntcies!
-                                                .map((e) => DropdownMenuItem(
-                                                      child: Text("${e.key}"),
-                                                      value: "${e.key}",
-                                                    ))
-                                                .toList(),
-                                            onChanged: (val) {
-                                              setState(() {
-                                                initalValue = val;
-                                              });
-                                            },
-                                          ),
-                                        ),
-                                        SizedBox(height: 50),
-                                        DropdownButton<dynamic>(
-                                          value: intvalue,
-                                          items: all_cuntcies!
-                                              .map((e) => DropdownMenuItem(
-                                                    child: Text("${e.key}"),
-                                                    value: "${e.key}",
-                                                  ))
-                                              .toList(),
-                                          onChanged: (val) {
+                                                  BorderRadius.circular(10),
+                                              color: Colors.lightBlue),
+                                          onSubmitted: (val) {
                                             setState(() {
-                                              intvalue = val;
+                                              if (val.isEmpty) {
+                                                amount_controller.text = "1";
+                                              } else {
+                                                amount_controller.text = val!;
+                                              }
                                             });
                                           },
+                                          keyboardType: TextInputType.number,
                                         ),
-                                      ],
-                                    );
-                                  }
-                                  return Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                },
-                              ),
-                            ],
-                          )),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                ":",
-                                style: style,
-                              ),
-                              SizedBox(height: 60),
-                              Text(
-                                ":",
-                                style: style,
-                              )
-                            ],
-                          ),
-                          SizedBox(width: 10),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                TextFormField(
-                                  controller: amount_controller,
-                                  onFieldSubmitted: (val) {
-                                    setState(() {
-                                      amount_controller.text = val!;
-                                    });
-                                  },
-                                  onSaved: (val) {},
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                    hintText: "Enter you ",
+                                      )
+                                    ],
                                   ),
-                                ),
-                                SizedBox(height: 70),
-                                FutureBuilder(
-                                  future: CurrenciAPIHelper.currenciAPIHelper
-                                      .fectdata(
-                                          parm:
-                                              num.parse(amount_controller.text),
-                                          from: initalValue,
-                                          to: intvalue),
-                                  builder: (context, snapshot) {
-                                    print(snapshot.data);
+                                  SizedBox(height: 30),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Result : ",
+                                        style: TextStyle(fontSize: 30),
+                                      ),
+                                      FutureBuilder(
+                                        future: CurrenciAPIHelper
+                                            .currenciAPIHelper
+                                            .fectdata(
+                                                parm: num.parse(
+                                                    amount_controller.text),
+                                                from: initalValue,
+                                                to: intvalue),
+                                        builder: (context, snapshot) {
+                                          print(snapshot.data);
 
-                                    if (snapshot.hasError) {
-                                      return Center(
-                                        child: Text("${snapshot.error}"),
-                                      );
-                                    } else if (snapshot.hasData) {
-                                      num? data = snapshot.data;
-                                      return Text(
-                                        "$data",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 22,
-                                            color: Colors.green),
-                                      );
-                                    } else {
-                                      return Center(
-                                        child: CircularProgressIndicator(),
-                                      );
-                                    }
-                                  },
-                                ),
-                                //
-                                Text(""),
-                              ],
-                            ),
-                          )
-                        ],
+                                          if (snapshot.hasError) {
+                                            return Center(
+                                              child: Text("${snapshot.error}"),
+                                            );
+                                          } else if (snapshot.hasData) {
+                                            num? data = snapshot.data;
+                                            return Text(
+                                              "$data",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 22,
+                                                  color: Colors.green),
+                                            );
+                                          } else {
+                                            return Center(
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            );
+                                          }
+                                        },
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
                       ),
                     ],
                   ),
